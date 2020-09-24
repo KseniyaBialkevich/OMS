3c798dc06b8b2c86bb607fdf3b01f1e9622d58df
Kseniya Bialkevich <kseniyabialkevich@gmail.com>
2020-09-24 21:36:15 +0300

    [dev] connection to technology and added create order handler

---
+++ init.sql
@@ -0,0 +1,36 @@
+sudo -u postgres psql
+\c oms_db
+\d
+
+CREATE DATABASE oms_db;
+
+CREATE TABLE menu (
+    id_menu SERIAL PRIMARY KEY, 
+    name VARCHAR(100) NOT NULL, 
+    price INTEGER NOT NULL
+);
+
+INSERT INTO menu  (name, price)
+    VALUES
+        ('pasta', 12),
+        ('steak', 26),
+        ('soup', 7),
+        ('potatoes', 4),
+        ('salad', 10),
+        ('water', 1),
+        ('coffee', 3);
+
+CREATE TABLE orders (
+    id_order SERIAL PRIMARY KEY, 
+    status VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE orders_to_menu (
+    id_order BIGINT NOT NULL, 
+    id_menu INTEGER NOT NULL, 
+    number INTEGER NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kspsql;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kspsql;
+
---
+++ server.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Menu struct {
+	ID_menu int    `json:"id_menu"`
+	Name    string `json:"name"`
+	Price   int    `json:"price"`
+}
+
+type Orders struct {
+	ID_order int    `json:"id_order"`
+	Status   string `json:"status"`
+}
+
+type OrdersToMenu struct {
+	ID_order int `json:"id_order"`
+	ID_menu  int `json:"id_menu"`
+	Number   int `json:"number"`
+}
+
+var database *sql.DB
+
+func CreateOrder(w http.ResponseWriter, r *http.Request) {
+	type OrderData struct {
+		ID_menu int `json:"id_menu"`
+		Number  int `json:"number"`
+	}
+
+	orderList := make([]OrderData, 0)
+
+	err := json.NewDecoder(r.Body).Decode(&orderList)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var id_order int
+
+	err = database.QueryRow("INSERT INTO orders (status) VALUES('recd') RETURNING id_order").Scan(&id_order)
+	if err != nil {
+		log.Println("Error -", err)
+		return
+	}
+
+	for _, order := range orderList {
+		_, err = database.Exec("INSERT INTO orders_to_menu (id_order, id_menu, number) VALUES ($1, $2, $3)", id_order, order.ID_menu, order.Number)
+		if err != nil {
+			log.Println("Error -", err)
+			return
+		}
+	}
+
+	w.Write([]byte("OK"))
+}
+
+func main() {
+
+	db, err := sql.Open("postgres", "user=kspsql password=pass1111 dbname=oms_db sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	database = db
+
+	defer db.Close()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/create_order", CreateOrder)
+
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Println("HTTP Server Error - ", err)
+	}
+}
