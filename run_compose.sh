f3dfd0cda5f0fa4cf701e36630eaf3019a22ed8d
Kseniya Bialkevich <kseniyabialkevich@gmail.com>
2020-10-16 22:17:05 +0300

    [dev] added docker-compose

---
+++ run_compose.sh
@@ -0,0 +1,15 @@
+sudo docker-compose build
+sudo docker-compose up -d
+
+sudo docker-compose down
+# docker-compose logs -f 
+
+
+
+#web:
+ #volumes:
+  # - .:/home/ks/Temp/docker_volum
+#db:
+ #volumes:
+  #- postgres_data:/home/ks/Temp/docker_volum/postgresql/data/
+
